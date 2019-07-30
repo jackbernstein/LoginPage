cb9a6be3d117ba3c1e3a2df5c03c232e6362ff43
Jack Bernstein <john.bernstein@pomona.edu>
2019-07-30 15:02:03 -0500

    First commit

---
+++ src/main/App.java
@@ -0,0 +1,52 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App {
+	
+	private final String url = "jdbc:postgresql://localhost:5432/sample_db";
+	
+
+	public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+ 
+        return conn;
+    }
+	
+	public void createUser(Connection conn, String email, 
+			String first, String last, String password) {
+		try {
+			if(email.isBlank() | first.isBlank() | last.isBlank() | password.isBlank()) {
+				System.out.println("Must include all information");
+				return;
+			} else {
+				Statement st = conn.createStatement();
+				st.executeUpdate("INSERT INTO users VALUES "
+						+ "('" + email + "', '" + first + "', '" + last + "', '" + password + "')");
+				System.out.println("User Created:");
+				System.out.println("Email: " + email);
+				System.out.println("Name: " + first + " " + last);
+				st.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("Email already exists");
+		}
+	}
+	
+	public static void main(String[] args) throws SQLException {
+		App app = new App();
+		Connection conn = app.connect();
+		app.createUser(conn, "mymail@gmail.com", "My", "Name", "0000");
+		
+	}
+
+}
